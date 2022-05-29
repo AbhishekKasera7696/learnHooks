55d9694a7fba5e6f64d25ead3d529b086ec6e05b
AbhishekKasera7696 <akasera62@gmail.com>
2022-05-29 19:34:50 +0530

    useRef

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import UseStateEx from "./useStatehook/UseStateEx";
 import UseReducerEx from "./useReducer/UseReducerEx";
 import UseEffectEx from './useEffect/UseEffectEx';
+import UseRefEx from './useRef/UseRefEx';
 
 function App() {
   return (
     <div className="App">
       {/* <UseStateEx/> */}
       {/* <UseReducerEx/> */}
-       <UseEffectEx/>
+       {/* <UseEffectEx/> */}
+       <UseRefEx/>
     </div>
   );
 }
