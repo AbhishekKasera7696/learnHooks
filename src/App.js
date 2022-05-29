a4b6f324a235c7dfe6a62dd25474b07150c2b39c
AbhishekKasera7696 <akasera62@gmail.com>
2022-05-29 19:12:32 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import UseStateEx from "./useStatehook/UseStateEx";
 import UseReducerEx from "./useReducer/UseReducerEx";
+import UseEffectEx from './useEffect/UseEffectEx';
 
 function App() {
   return (
     <div className="App">
       {/* <UseStateEx/> */}
-      <UseReducerEx/>
+      {/* <UseReducerEx/> */}
+       <UseEffectEx/>
     </div>
   );
 }
---
+++ src/useEffect/UseEffectEx.js
@@ -0,0 +1,28 @@
+import React , {useEffect, useState} from 'react';
+import axios from "axios";
+
+
+const UseEffectEx = () => {
+    const [data, setData] = useState("");
+    const [count, setCount] = useState(0);
+
+  useEffect(() => {
+      axios.get("https://jsonplaceholder.typicode.com/comments").then((res) => {
+        setData(res.data[0].email);
+        console.log("API WAS CALLED")
+      }) 
+  }, [count])
+
+  return (
+    <div>
+        effect 
+        <h1>{data}</h1>
+        <h1>{count}</h1>
+        <button onClick={() => {
+            setCount(count + 1)
+        }}>Click</button>
+    </div>
+  )
+}
+
+export default UseEffectEx
